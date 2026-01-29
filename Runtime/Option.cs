f30fb8792f05520cde722b34087d13cd3a4092e2
ACELANDWORKSHOP\parsu <parsue@gmail.com>
2026-01-29 22:19:31 +0800

    implicit operator bool for null checking

--- Runtime/Option.cs
+++ Runtime/Option.cs
@@ -10,7 +10,6 @@ namespace AceLand.Optional
         public static Option<T> Some(T obj) => new() { _content = obj };
         public static Option<T> None() => new();
 
-        public bool IsPresent() => _content is not null;
         public T? Get() => _content;
         
         public readonly Option<TResult> Map<TResult>(Func<T, TResult> map) where TResult : class =>
@@ -37,10 +36,9 @@ namespace AceLand.Optional
         
         public override string ToString() => _content is null ? "null" : _content.ToString();
 
-        public bool Equals(Option<T> other) =>
-            _content is null ? other._content is null
-                : _content.Equals(other._content);
+        public bool Equals(Option<T> other) => _content?.Equals(other._content) ?? other._content is null;
 
+        public static implicit operator bool(Option<T>? value) => value?._content is not null;
         public static implicit operator Option<T>(T? value) => value is null ? None() : Some(value);
         public static bool operator ==(Option<T>? a, Option<T>? b) => a is null ? b is null : a.Equals(b);
         public static bool operator !=(Option<T>? a, Option<T>? b) => !(a == b);
--- Runtime/ValueOption.cs
+++ Runtime/ValueOption.cs
@@ -10,7 +10,6 @@ namespace AceLand.Optional
         public static ValueOption<T> Some(T? value) => new() { _content = value };
         public static ValueOption<T> None() => new();
 
-        public bool IsPresent() => _content is not null;
         public T? Get() => _content;
 
         public Option<TResult> Map<TResult>(Func<T, TResult> map) where TResult : class =>
@@ -41,6 +40,7 @@ namespace AceLand.Optional
             _content.HasValue ? other._content.HasValue && _content.Value.Equals(other._content.Value)
                 : !other._content.HasValue;
 
+        public static implicit operator bool(ValueOption<T>? value) => value?._content is not null;
         public static implicit operator ValueOption<T>(T? value) => value is null ? None() : Some(value);
         public static bool operator ==(ValueOption<T> a, ValueOption<T> b) => a.Equals(b);
         public static bool operator !=(ValueOption<T> a, ValueOption<T> b) => !(a.Equals(b));
